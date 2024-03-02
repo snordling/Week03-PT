193557b4ebf8ce21dc923faec42f1296862ba678
Suma Raghavendran <suma.raghavendran@gmail.com>
2024-03-02 16:03:55 -0600

    update

---
+++ src/Week3/Week3.java
@@ -0,0 +1,19 @@
+package Week3;
+
+public class Week3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;
+		increment(x);
+		System.out.println(x);
+				
+	}
+	
+	public static void increment(int x) {
+		
+		x++;
+		System.out.println(x);
+	}
+
+}
---
+++ src/Week3/Week4.java
@@ -0,0 +1,19 @@
+package Week3;
+
+public class Week4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;
+		increment(x);
+		System.out.println(x);
+				
+	}
+	
+	public static void increment(int x) {
+		
+		x++;
+		System.out.println(x);
+	}
+
+}
